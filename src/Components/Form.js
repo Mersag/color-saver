cecfc8ef60dd52c7a4e504c79f5246b5165a4a83
Meral Sager <mersa22@gmx.de>
2022-07-25 13:00:04 +0200

    Add OnChange and setColor

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import "./Form.css";
-export default function Form({ color }) {
+export default function Form({}) {
+  const [color, setColor] = useState("#57E2E5");
   return (
     <div className="FormContainer">
       <form
@@ -8,8 +10,22 @@ export default function Form({ color }) {
           event.preventDefault();
         }}
       >
-        <input type="color" className="farbeInput" />
-        <input type="text" className="hexInput" />
+        <input
+          type="color"
+          className="farbeInput"
+          value={color}
+          onChange={(event) => {
+            setColor(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          className="hexInput"
+          value={color}
+          onChange={(event) => {
+            setColor(event.target.value);
+          }}
+        />
         <button label="colorButton">Add Color</button>
       </form>
     </div>
